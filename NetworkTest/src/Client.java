8541a06c80a6973a0032f3d19d7ba7e4e741a58d
feel2174 <feel2174@gmail.com>
2018-12-02 20:46:25 +0900

    server commit2

---
+++ NetworkTest/src/Client.java
@@ -0,0 +1,33 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) {
+		try(Socket client = new Socket()) {
+			InetSocketAddress ipep = new InetSocketAddress("35.241.181.46", 80);
+			client.connect(ipep);
+			
+			try(OutputStream sender = client.getOutputStream();
+				InputStream receiver = client.getInputStream();) {
+				byte[] data = new byte[11];
+				receiver.read(data, 0, 11);
+				
+				String message = new String(data);
+				String out = String.format("receive - %s", message);
+				System.out.println(out);
+				
+				message = "OK";
+				data = message.getBytes();
+				sender.write(data, 0, data.length);
+				
+			}
+		} catch(Throwable e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
