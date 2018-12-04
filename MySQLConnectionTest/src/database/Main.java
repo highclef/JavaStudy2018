bf09f89d3e281308e878604defdada31b9293ed7
highclef <high_clef@naver.com>
2018-12-04 20:15:09 +0100

    commit

---
+++ MySQLConnectionTest/src/database/Main.java
@@ -0,0 +1,16 @@
+package database;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Query query = new Query();
+				
+		query.getTableName();
+		
+		query.search();
+
+		
+
+	}
+
+}
