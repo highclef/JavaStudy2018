b0f5cb5d0436991bef38277bc88638db72376818
highclef <high_clef@naver.com>
2018-12-10 23:54:37 +0100

    Parsing JSON Into Java Objects

--- Object2Json/src/test1/Converter.java
+++ Object2Json/src/test1/Converter.java
@@ -9,11 +9,11 @@ public class Converter {
 	public static void main(String[] args) {
 		Member member = Member.getMember();
 		
-		System.out.println("Parsing JSON Into Java Objects\n");
+		System.out.println("Generating JSON From Java Objects\n");
 		String jsonString = new Gson().toJson(member);
         System.out.println(jsonString);
         
-        System.out.println("\nGenerating JSON From Java Objects\n");
+        System.out.println("\nParsing JSON Into Java Objects\n");
         Member convertedJson = new Gson().fromJson(jsonString, Member.class);
         printMemberObject(convertedJson);
 	}
