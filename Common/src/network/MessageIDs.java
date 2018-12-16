a423c92068c4002c9e46e2b57b45023083229d3c
myjun <myjun@LAPTOP-A8VNLJ9O.fritz.box>
2018-12-16 20:39:31 +0100

    오타 수정

--- Common/src/network/MessageIDs.java
+++ Common/src/network/MessageIDs.java
@@ -7,6 +7,6 @@ public class MessageIDs {
 	public static final int UPDATEPOSTINGDATA_REQ = 0x00000004; //data : PostingModel
 	
 	public static final int ADDPOSTRINGDATA_RES = 0x00010001; // data : PostingModel
-	public static final int DELPoSTINGDATA_RES = 0x00010003; // data : int(id)
+	public static final int DELPOSTINGDATA_RES = 0x00010003; // data : int(id)
 	public static final int UPDATEPOSTINGDATA_RES = 0x00010004; //data : PostingModel
 }
