b01075ca5b6aa29d3d0876c89e3dc76102fd229a
myjun <myjun@LAPTOP-A8VNLJ9O.fritz.box>
2018-12-16 20:02:08 +0100

    delete, update postingdata 메시지 추가

--- Common/src/network/MessageIDs.java
+++ Common/src/network/MessageIDs.java
@@ -3,6 +3,10 @@ package network;
 public class MessageIDs {
 	public static final int ADDPOSTINGDATA_REQ = 0x00000001; // data : PostingModel
 	public static final int POSTINGDATALIST_REQ = 0x00000002; // data : none
+	public static final int DELPOSTINGDATA_REQ = 0x00000003; // data : int(id)
+	public static final int UPDATEPOSTINGDATA_REQ = 0x00000004; //data : PostingModel
 	
 	public static final int ADDPOSTRINGDATA_RES = 0x00010001; // data : PostingModel
+	public static final int DELPoSTINGDATA_RES = 0x00010003; // data : int(id)
+	public static final int UPDATEPOSTINGDATA_RES = 0x00010004; //data : PostingModel
 }
